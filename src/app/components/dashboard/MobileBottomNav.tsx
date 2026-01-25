0f2c54f5b37e31ddfb753ba2d74f91e12c1a7fd8
Anna Bars <abarseghyan863@gmail.com>
2026-01-26 03:23:17 +0400

    FEAT: Added border top to mobile navigation

--- src/app/components/dashboard/MobileBottomNav.tsx
+++ src/app/components/dashboard/MobileBottomNav.tsx
@@ -22,7 +22,7 @@ export default function MobileBottomNav({
   const isActive = (itemLabel: string) => activeNavItem === itemLabel
   
   return (
-    <div className="xl:hidden fixed bottom-0 left-0 right-0 z-40 bg-[#1a1a1a] py-1">
+    <div className="xl:hidden fixed border-t border-[#1a1a1a] bottom-0 left-0 right-0 z-40 bg-[#1a1a1a] py-1">
       <img src="/nav/mob-right-top.svg" className='absolute -top-[28.5px] right-0' alt="" />
       <img src="/nav/mob-left-top.svg" className='absolute -top-[28.5px] left-0' alt="" />
       <div className="flex justify-between items-center h-[70px] px-4">
